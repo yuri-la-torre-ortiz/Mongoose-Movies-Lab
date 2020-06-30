44b6e93dec32583fd348225f6f4ccad0fbba9d7c
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-30 15:31:30 +0200

    Fix create celebrity function bug

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,15 +2,14 @@ const express = require('express');
 const Celebrity = require('../models/celebrity');
 const router = express.Router();
 
-router.get('/celebrities', (req, res, next) => {
+router.get('/celebrities/', (req, res, next) => {
   // get all the celebrities from the database
   Celebrity.find().then(celebritiesFromDatabase => {
     // render a 'celebrities' view with the celebrities data
     console.log(celebritiesFromDatabase);
     res.render('celebrities/index', { celebrities: celebritiesFromDatabase });
   }).catch(err => {
-    next();
-    console.log(err);
+    return next(new Error("Error produced while searching for celebrities: " + err));
   })
 });
 
@@ -18,20 +17,13 @@ router.get('/celebrities/:celebrityId', (req, res, next) => {
   // get celebrity by ID from the database
   const celebrityId = req.params.celebrityId
   Celebrity.findById(celebrityId).then(celebrityFromDatabase => {
-/*     if (err) {
-      
-      return;
-    }
-    // render a 'celebrities' view with the */ //celebrities data
-    console.log(celebrityFromDatabase);
     res.render('celebrities/show', { celebrity: celebrityFromDatabase });
   }).catch(err => {
-    next();
-    console.log(err);
+    return next(new Error("Couldn't find celebrity: " + err));
   })
 });
 
-router.get('/celebrities/new', (req, res, next) => {
+router.get('/new/', (req, res, next) => {
   res.render('celebrities/new');
 });
 
