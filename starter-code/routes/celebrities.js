be79c36e0afb8274c0f272b6193518b730a5eebf
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-30 12:12:45 +0200

    Render newly created celeb details

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -32,7 +32,25 @@ router.get('/celebrities/:celebrityId', (req, res, next) => {
 });
 
 router.get('/celebrities/new', (req, res, next) => {
-  res.render('/celebrities/new');
+  res.render('celebrities/new');
 });
+
+router.post('/celebrities/', (req, res) => {
+  console.log(req.body);
+  // const name = req.body.name;
+  // const occupation = req.body.occupation;
+  // const catchPhrase = req.body.catchPhrase;
+  const { name, occupation, catchPhrase} = req.body;
+  Celebrity.create({
+    name, occupation, catchPhrase
+  }).then(celebrity => {
+    console.log(`Success! ${celebrity.name} was added to the database.`);
+    celebrity.save();
+    res.redirect(`/celebrities/${celebrity._id}`);
+  }).catch(err => {
+    console.log(err);
+    res.redirect('celebrities/new');
+  })
+})
 
 module.exports = router;
