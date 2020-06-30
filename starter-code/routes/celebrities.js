746049ead751a2c32ddda78b6f56e656565f8596
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-30 11:13:20 +0200

    Render Celebrity Details page

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Celebrity = require('../models/celebrity');
+const router = express.Router();
+
+router.get('/celebrities', (req, res, next) => {
+  // get all the celebrities from the database
+  Celebrity.find().then(celebritiesFromDatabase => {
+    // render a 'celebrities' view with the celebrities data
+    console.log(celebritiesFromDatabase);
+    res.render('celebrities/index', { celebrities: celebritiesFromDatabase });
+  }).catch(err => {
+    next();
+    console.log(err);
+  })
+});
+
+router.get('/celebrities/:celebrityId', (req, res, next) => {
+  // get celebrity by ID from the database
+  const celebrityId = req.params.celebrityId
+  Celebrity.findById(celebrityId).then(celebrityFromDatabase => {
+/*     if (err) {
+      next(new Error("Couldn't find celebrity: " + err));
+      return;
+    }
+    // render a 'celebrities' view with the */ //celebrities data
+    console.log(celebrityFromDatabase);
+    res.render('celebrities/show', { celebrity: celebrityFromDatabase });
+  }).catch(err => {
+    next();
+    console.log(err);
+  })
+});
+
+
+
+module.exports = router;
